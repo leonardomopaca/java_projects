24d381da1f9f8c9766185fab4edd26185ff8f08e
leonardomopaca <43859947+leonardomopaca@users.noreply.github.com>
2020-06-22 13:04:02 -0500

    Update PessoaService.java

--- pessoa-service/src/main/java/br/com/leo/pessoaservice/service/PessoaService.java
+++ pessoa-service/src/main/java/br/com/leo/pessoaservice/service/PessoaService.java
@@ -1,4 +1,6 @@
 package br.com.leo.pessoaservice.service;
 
 public class PessoaService {
+    //url de onde eu parei na codificação da PessoaService
+    //http://www.ciceroednilson.com.br/desenvolvendo-uma-aplicacao-web-com-angular-4-e-spring-boot-parte-3-criando-o-servico-rest-com-spring-boot/
 }
