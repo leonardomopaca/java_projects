221b6a56fb750fb555afd6ec9c3022074586fc40
leonardomopaca <43859947+leonardomopaca@users.noreply.github.com>
2020-06-26 10:40:41 -0500

    Atualização ExemploQueue
    Definição de ações com os métodos, poll, peek e element

--- ganimedes/src/main/java/com/leo/dio/queue/ExampleQueue.java
+++ ganimedes/src/main/java/com/leo/dio/queue/ExampleQueue.java
@@ -10,5 +10,34 @@ public class ExampleQueue {
         nomes.add("BCD");
         nomes.add("CDE");
         nomes.add("DEF");
+
+        System.out.println(nomes.poll());
+
+        System.out.println(nomes);
+
+        nomes.add("EFG");
+
+        System.out.println(nomes.peek());
+        System.out.println(nomes);
+
+        System.out.println(nomes.element());
+        System.out.println(nomes);
+
+        nomes.clear();
+
+        System.out.println(nomes.peek());
+        System.out.println(nomes);
+
+        //nomes.add("FGH");
+
+        try {
+            System.out.println(nomes.element());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("\nA fila está vazia, erro lançado pelo método element()");
+        }
+
+
     }
 }
